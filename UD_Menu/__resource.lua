53666696f4bfb2dde41077325558b4f0ffdbe328
Mart475 <49720546+Mart475@users.noreply.github.com>
2019-09-28 22:36:03 +1000

    Update __resource.lua

--- UD_Menu/__resource.lua
+++ UD_Menu/__resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-name "Universal Development Menu (M Menu)"
-author "DevDawson - Universal Development"
+name "Universal Development Menu (M Menu)."
+author "DevDawson - Universal Development."
 description "The UD Menu (Universal Development Menu) is a useful 'M' Menu that will help all roleplaying servers with it's various features for LEO to Civilians."
 
 client_scripts {
